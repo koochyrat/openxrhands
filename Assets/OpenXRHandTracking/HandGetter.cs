09c76d2a7adf64c8ce200619f586e79873d7f8b3
araghon007 <araghon0071@gmail.com>
2022-05-14 19:00:53 +0200

    Added calculations to get finger curl values

--- Assets/OpenXRHandTracking/HandGetter.cs
+++ Assets/OpenXRHandTracking/HandGetter.cs
@@ -1,24 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
 using UnityEngine.XR.OpenXR;
+
 public class HandGetter : MonoBehaviour
 {
-
-    GameObject lhand,rhand;
-
     // Start is called before the first frame update
-    void Start()
-    {
-    }
 
-    public float rx,ry,rz;
-    public Material mat;
     public Vector3[] positions;
     public Quaternion[] orientations;
     public Transform[] joints;
     public float[] radius;
+
+#if DEBUG
+    public GameObject DebugJointMesh;
+#endif
+
+    public GameObject WristObject;
+
+    private float[] _localCurlsOpen = new float[5]
+    {
+        -83.6991f,
+        3.24045038f,
+        9.166323f,
+        17.0771484f,
+        12.6282425f
+    };
+
+    private float[] _localCurlsClosed = new float[5]
+    {
+        99.27916f,
+        286.103638f,
+        292.402283f,
+        294.4036f,
+        289.7828f
+    };
+
+    public float[] Curls = new float[5];
+
     // Update is called once per frame
     void Update()
     {
@@ -30,7 +47,6 @@ public class HandGetter : MonoBehaviour
         
         if(hf)
         {
-            //float[] radius;
             hf.GetHandJoints(HandTrackingFeature.Hand_Index.R, out positions, out orientations, out radius);
             if (positions.Length == 0) return;
             if(joints == null || joints.Length == 0)
@@ -40,8 +56,28 @@ public class HandGetter : MonoBehaviour
                 {
                     joints[i] = new GameObject("Joint").transform;
                     joints[i].parent = transform;
+
+                    #if DEBUG
+                        if(DebugJointMesh != null)
+                        {
+                            GameObject obj = Instantiate(DebugJointMesh);
+                            obj.transform.localScale = Vector3.one * radius[i] * 10;
+                            obj.transform.parent = joints[i];
+                            obj.transform.localPosition = Vector3.zero;
+                            obj.transform.localRotation = Quaternion.identity;
+                        }
+                    #endif
                 }
             }
+            
+            // Transform orientations back into SteamVR skeleton space
+            orientations[1] = orientations[1] * Quaternion.Euler(0, 0, 90); // Wrist is aligned to a different axis.. Don't ask me why
+
+            for (int i = 2; i < orientations.Length; i++)
+            {
+                orientations[i] = orientations[i] * Quaternion.Euler(0, -90, 0);
+            }            
+
             for (int i = 0; i < positions.Length; i++)
             {
                 joints[i].transform.position = positions[i];
@@ -51,6 +87,12 @@ public class HandGetter : MonoBehaviour
                     case 1: //wrist
                         transform.position = joints[i].transform.position;
                         transform.rotation = joints[i].transform.rotation;
+                        
+                        if(WristObject != null)
+                        {
+                            WristObject.transform.position = transform.position;
+                            WristObject.transform.rotation = transform.rotation;
+                        }
                         break;
                     case 0: //palm
                         Debug.DrawLine(joints[1].transform.position, joints[i].transform.position, Color.red);
@@ -64,21 +106,52 @@ public class HandGetter : MonoBehaviour
                 }
             }
 
+            float[] localCurls = new float[5];
+
+            for(int i = 0; i < orientations.Length; i++)
+            {
+                if (i == 0)
+                    continue;
+
+                var localOrientation = Quaternion.Inverse(orientations[i-1]) *  orientations[i];
+
+                switch (i)
+                {
+                    case 2: case 3: case 4: // thumb
+                        localCurls[0] -= ConvertAngle(localOrientation.eulerAngles.z);
+                        break;
+                    case 7: case 8: case 9: // index
+                        localCurls[1] -= ConvertAngle(localOrientation.eulerAngles.z);
+                        break;
+                    case 12: case 13: case 14: // middle
+                        localCurls[2] -= ConvertAngle(localOrientation.eulerAngles.z);
+                        break;
+                    case 17: case 18: case 19: // ring
+                        localCurls[3] -= ConvertAngle(localOrientation.eulerAngles.z);
+                        break;
+                    case 22: case 23: case 24: // pinky
+                        localCurls[4] -= ConvertAngle(localOrientation.eulerAngles.z);
+                        break;
+                }
+            }
+
+            for(int i = 0; i < 5; i++)
+            {
+                Curls[i] = RemapClamped(localCurls[i], _localCurlsOpen[i], _localCurlsClosed[i]);
+            }
         }
-        /*if(lhand==null)
-        {
-            hf.GetHandMesh(HandTrackingFeature.Hand_Index.L,transform,mat,out lhand);
-        }
-        else
-        {
-            hf.ApplyHandJointsToMesh(HandTrackingFeature.Hand_Index.L,lhand,rx,ry,rz);
-        }    
-        if(rhand==null)
-        {
-            hf.GetHandMesh(HandTrackingFeature.Hand_Index.R,transform,mat,out rhand);            
-        }else
-        {
-            hf.ApplyHandJointsToMesh(HandTrackingFeature.Hand_Index.R,rhand,rx,ry,rz);
-        }*/
+    }
+
+    public static float RemapClamped(float value, float in1, float in2)
+    {
+        float t = (value - in1) / (in2 - in1);
+        return Mathf.Clamp01(t);
+    }
+
+    public float ConvertAngle(float angle)
+    {
+        if (angle > 180f)
+            angle -= 360f;
+        return angle;
     }
 }
